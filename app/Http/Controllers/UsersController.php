b3f2dbe2d4fd740439bda6d7b643522cbbe1081e
tomonobu <tomonobu.sasaki@bravi.co.jp>
2017-10-18 09:47:40 +0000

    post

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -27,5 +27,20 @@ class UsersController extends Controller
             'user' => $user,
         ]);
     }
+    public function show($id)
+    {
+        $user = User::find($id);
+        $microposts = $user->microposts()->orderBy('created_at', 'desc')->paginate(10);
+        $count_microposts = $user->microposts()->count();
+        
+        $data = [
+            'user' => $user,
+            'microposts' => $microposts,
+        ];
+        
+        $data += $this->counts($user);
+        
+        return view('users.show', $data);
+    }
     
 }
--- app/Http/routes.php
+++ app/Http/routes.php
@@ -10,6 +10,7 @@
 | and give it the controller to call when that URI is requested.
 |
 */
+Route::get('/', 'WelcomeController@index');
 // ユーザ登録
 Route::get('signup', 'Auth\AuthController@getRegister')->name('signup.get');
 Route::post('signup', 'Auth\AuthController@postRegister')->name('signup.post');
@@ -22,4 +23,5 @@ Route::get('logout', 'Auth\AuthController@getLogout')->name('logout.get');
 
 Route::group(['middleware' => 'auth'], function () {
     Route::resource('users', 'UsersController', ['only' => ['index', 'show']]);
+    Route::resource('microposts', 'MicropostsController', ['only' => ['store', 'destroy']]);
 });
