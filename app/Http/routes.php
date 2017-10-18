8f1c7c22735e06d65173b954e7ca24b0bda2c3e4
tomonobu <tomonobu.sasaki@bravi.co.jp>
2017-10-18 03:39:15 +0000

    user registration

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -10,8 +10,6 @@
 | and give it the controller to call when that URI is requested.
 |
 */
-//Route::get('/', 'MessagesController@index');
-
-Route::get('/', function () {
-    return view('welcome');
-});
+// ユーザ登録
+Route::get('signup', 'Auth\AuthController@getRegister')->name('signup.get');
+Route::post('signup', 'Auth\AuthController@postRegister')->name('signup.post');
